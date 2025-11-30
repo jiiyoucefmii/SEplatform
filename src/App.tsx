fe92cf4ada1b05425146c863cbdc88f3f4539405
dhohaKHELLAF <dhoha.khellaf@ensia.edu.dz>
2025-11-30 20:37:46 +0100

    feat: student dashboard , signup , sign in pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import TeacherDashboard from "./pages/TeacherDashboard";
-import DashboardPage from "./pages/StudentDashboard";
+import DashboardPage from "./pages/Dashboard";
 import SignUpPage from "./pages/SignupPage";
 
 function App() {
   return (
     <BrowserRouter>
-     <Routes>
-  <Route path="/login" element={<LoginPage />} />
-  <Route path="/signup" element={<SignUpPage />} />
-  <Route path="/dashboard" element={<DashboardPage />} />
-  <Route path="/teacher" element={<TeacherDashboard />} />  {/* ✅ add route */}
-  <Route path="/" element={<Navigate to="/teacher" replace />} />  {/* ✅ redirect */}
-</Routes>
-
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,3 @@
-
 interface InputProps {
   type: string;
   placeholder: string;
